be9c101cd9139f75ef858e2b8ec7b8470e106c68
Svante Signell <svante.signell@gmail.com>
2022-11-24 21:15:54 +0100

    Bug 1796917 - Parenthesis error in nspr/pr/src/pthreads/ptsynch.c. r=kaie

--- pr/src/pthreads/ptsynch.c
+++ pr/src/pthreads/ptsynch.c
@@ -50,7 +50,7 @@ void _PR_InitLocks(void)
     rv = _PT_PTHREAD_MUTEXATTR_INIT(&_pt_mattr);
     PR_ASSERT(0 == rv);
 
-#if (defined(LINUX) && (__GLIBC__ > 2) || (__GLIBC__ == 2 && __GLIBC_MINOR__ >= 2)) || \
+#if (defined(LINUX) && (__GLIBC__ > 2 || (__GLIBC__ == 2 && __GLIBC_MINOR__ >= 2))) || \
     (defined(FREEBSD) && __FreeBSD_version > 700055)
     rv = pthread_mutexattr_settype(&_pt_mattr, PTHREAD_MUTEX_ADAPTIVE_NP);
     PR_ASSERT(0 == rv);
