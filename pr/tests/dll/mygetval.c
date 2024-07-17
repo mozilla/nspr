8e72fd2b725cff3896ce803030b4780991c81047
Sylvestre Ledru <sledru@mozilla.com>
2024-07-17 14:26:19 +0000

    Bug 1908336 - Reformat the code using clang r=kaie
    With clang-format version 18.1.7 (taskcluster-aGjHEzhUS9CKHrxfBw0O6Q)
    
    # ignore-this-changeset
    
    Differential Revision: https://phabricator.services.mozilla.com/D216804

--- pr/tests/dll/mygetval.c
+++ pr/tests/dll/mygetval.c
@@ -3,12 +3,8 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-
 #include "prtypes.h"
 
 extern PRIntn my_global;
 
-PR_IMPLEMENT(PRIntn) My_GetValue()
-{
-    return my_global;
-}
+PR_IMPLEMENT(PRIntn) My_GetValue() { return my_global; }
