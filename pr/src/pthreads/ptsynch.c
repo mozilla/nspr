deb7ada426b8baf98b2799e5622f200468932d81
Sylvestre Ledru <sledru@mozilla.com>
2024-07-18 16:11:01 +0000

    Bug 1908336 - unbreak the NSS mac build r=kaie
    + reformat the rest of the file
    
    Differential Revision: https://phabricator.services.mozilla.com/D216972

--- pr/src/pthreads/ptsynch.c
+++ pr/src/pthreads/ptsynch.c
@@ -863,8 +863,8 @@ PR_IMPLEMENT(PRStatus) PR_DeleteSemaphore(const char* name) {
  * From the semctl(2) man page in glibc 2.0
  */
 #    if (defined(__GNU_LIBRARY__) && !defined(_SEM_SEMUN_UNDEFINED)) || \
-        (defined(FREEBSD) && __FreeBSD_version < 1200059) || defined(OPENBSD)
-|| defined(DARWIN)
+        (defined(FREEBSD) && __FreeBSD_version < 1200059) ||            \
+        defined(OPENBSD) || defined(DARWIN)
 /* union semun is defined by including <sys/sem.h> */
 #    else
 /* according to X/OPEN we have to define it ourselves */
@@ -882,8 +882,8 @@ union semun {
 
 #    define NSPR_SEM_MODE 0666
 
-        PR_IMPLEMENT(PRSem*) PR_OpenSemaphore(const char* name, PRIntn flags,
-                                              PRIntn mode, PRUintn value) {
+PR_IMPLEMENT(PRSem*)
+PR_OpenSemaphore(const char* name, PRIntn flags, PRIntn mode, PRUintn value) {
   PRSem* sem;
   key_t key;
   union semun arg;
