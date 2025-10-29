a5f228bcb6fa083835e422bc4d93c35b0c39a4d9
Kai Engert <kaie@kuix.de>
2025-10-29 12:58:37 +0100

    Backed out changeset 12f86bb88cfb - accidental commit

--- pr/src/pthreads/ptio.c
+++ pr/src/pthreads/ptio.c
@@ -1923,7 +1923,8 @@ static PRInt32 pt_RecvFrom(PRFileDesc* fd, void* buf, PRInt32 amount,
                    (struct sockaddr*)addr, &addr_len);
   syserrno = errno;
 
-  if ((bytes == -1) && (syserrno == EWOULDBLOCK || syserrno == EAGAIN)) {
+  if ((bytes == -1) && (syserrno == EWOULDBLOCK || syserrno == EAGAIN) &&
+      (!fd->secret->nonblocking)) {
     if (PR_INTERVAL_NO_WAIT == timeout) {
       syserrno = ETIMEDOUT;
     } else {
